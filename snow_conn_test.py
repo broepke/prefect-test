e1013e4725dccbd166a42430cf5421206d93733a
Brian Roepke <broepke@gmail.com>
2023-01-18 09:50:58 -0600

    Basic Working Lamba API Call

--- snow_conn_test.py
+++ snow_conn_test.py
@@ -1,21 +1,68 @@
-import pprint
-
-from prefect import flow
+import requests
+from prefect import flow, task
+from prefect import flow, get_run_logger
 from prefect_snowflake.database import SnowflakeConnector
 from prefect_snowflake.database import snowflake_query
 
 snowflake_connector_block = SnowflakeConnector.load("fxdmz")
 
+@task
+def get_imdb_list(result):
+    
+    id_list = []
+    
+    for i in range(len(result)):
+        id_list.append(result[i][2])
+    
+    return id_list
+
+@task
+def built_url(id_list):
+    
+    url = "https://t241zyf9j9.execute-api.us-east-1.amazonaws.com/default/lamba-tmdb?"
+    
+    for i in range(len(id_list)):
+        url += "ids="
+        url += id_list[i]
+        if i == len(id_list)-1:
+            return url
+        else:
+            url += "&"
+    
+    return url
+
 @flow
 def snowflake_query_flow():
     
-    query = """select * from orgs where org_id = '4911a544-e3be-4f16-b2f6-92e9ee166ab1';"""
+    logger = get_run_logger()
+    
+    query = """
+    SELECT ORG_ID, CREATIVEWORKID, IMDB_VALUE
+    FROM EIDR.PUBLIC.ORGS_IMDB_FILTERED
+    ORDER BY RANDOM()
+    LIMIT 3;
+    """
     
     result = snowflake_query(
         query,
         snowflake_connector_block,
         params={"id_param": 1}
     )
-    return result
+    
+    id_list = get_imdb_list(result)
+    url = built_url(id_list)
+    
+    logger.info(url)
+    
+    # Call the Lambda function
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return logger.info(response.status_code)
+    else:
+        return logger.warning(response.status_code)
 
-pprint.pprint(snowflake_query_flow())
+
+
+if __name__ == "__main__":
+    snowflake_query_flow()

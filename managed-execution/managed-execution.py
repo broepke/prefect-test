1d71895ca59bb0ce8cadd4885c62f708b2323245
Brian Roepke <broepke@gmail.com>
2024-02-15 08:02:15 -0600

    Managed Execution Tutorial / Test

--- managed-execution/managed-execution.py
+++ managed-execution/managed-execution.py
@@ -2,9 +2,10 @@ from prefect import flow
 
 if __name__ == "__main__":
     flow.from_source(
-    source="https://github.com/desertaxle/demo.git",
-    entrypoint="flow.py:my_flow",
+        source="https://github.com/desertaxle/demo.git",
+        entrypoint="flow.py:my_flow",
     ).deploy(
         name="test-managed-flow",
         work_pool_name="my-managed-pool",
+        job_variables={"pip_packages": ["pandas", "prefect-aws"]},
     )
